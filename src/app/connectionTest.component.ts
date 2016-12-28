04538433142ced6f4d0ef906e0f179064c680e86
mekoo <michael.jespersen@gmail.com>
2016-12-28 11:17:35 +0100

    updated wsuri

--- src/app/connectionTest.component.ts
+++ src/app/connectionTest.component.ts
@@ -22,7 +22,7 @@ export class ConnectionTestComponent {
         
         //const url:string = "ws://127.0.0.1:8082/ws";
         let url:string;
-        if (document.location.host === 'udm.herokuapp.com') {
+        if (document.location.host === 'mikou.github.io') {
             url = (document.location.protocol === "http:" ? "ws" : "wss") + "//udm.herokuapp.com/ws";
         } else {
             url = (document.location.protocol === "http:" ? "ws" : "wss") + "//" +
